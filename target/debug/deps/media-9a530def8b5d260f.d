4943b305d5892bd797cef9191f92bebcb3361d76
Chirag <chiragnabhoya2506@gmail.com>
2025-02-21 13:13:12 +0100

    initialized project

---
+++ target/debug/deps/media-9a530def8b5d260f.d
@@ -0,0 +1,5 @@
+C:\Users\chira\Desktop\rust_practice\media\target\debug\deps\libmedia-9a530def8b5d260f.rmeta: src/main.rs
+
+C:\Users\chira\Desktop\rust_practice\media\target\debug\deps\media-9a530def8b5d260f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/media-f629c4344fe79bd8.d
@@ -0,0 +1,5 @@
+C:\Users\chira\Desktop\rust_practice\media\target\debug\deps\libmedia-f629c4344fe79bd8.rmeta: src/main.rs
+
+C:\Users\chira\Desktop\rust_practice\media\target\debug\deps\media-f629c4344fe79bd8.d: src/main.rs
+
+src/main.rs:
